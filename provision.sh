b2208ceae05ef1394b7de0e8da3437dba645f221
Gareth Evans <evans.g.p@gmail.com>
2018-03-03 15:01:18 +0800

    add configurable pi host

--- provision.sh
+++ provision.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-# Put SDR application software onto a naked raspbian-jessie-lite
+# Put software onto a raspbian imaged rpi
 
 set -e
 set -u
 
+host=
+
+for i in "$@"
+do
+case $i in
+    -h=*|--host=*)
+    host="${i#*=}"
+    shift
+esac
+done
+
+[ -z "$host" ] && echo "rpi host:" && read host
+
 # enable only ssh key auth for pi@rpi
 echo "Default password from ISO is 'raspberry'"
-ssh-copy-id pi@rpi
-scp bootstrap.sh pi@rpi:/tmp/bootstrap.sh
-ssh pi@rpi sudo /bin/bash /tmp/bootstrap.sh
+ssh-copy-id "pi@$host"
+scp bootstrap.sh pi@$host:/tmp/bootstrap.sh
+ssh pi@$host sudo /bin/bash /tmp/bootstrap.sh
